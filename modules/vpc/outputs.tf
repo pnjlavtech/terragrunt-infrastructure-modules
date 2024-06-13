70bfaba3e059bb5ba4b9c65b2dd5ebbd72575b1f
Jay Lavine <jaylavine@gmail.com>
2024-06-12 21:33:44 -0700

    VPC module add fix outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,6 +1,6 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = module.vpc.id
+  value       = module.vpc.vpc_id
 }
 
 output "vpc_arn" {
@@ -11,6 +11,11 @@ output "vpc_arn" {
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
   value       = module.vpc.vpc_cidr_block
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = module.vpc.default_security_group_id
 }
 
 output "public_subnets" {
