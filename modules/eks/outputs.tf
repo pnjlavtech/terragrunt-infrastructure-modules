c104ffec32ad026261d4104a15b21688d8f68b5c
Jay Lavine <jaylavine@gmail.com>
2024-06-16 22:57:57 -0700

    EKS & IAM module, PATCH, upd outputs

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_certificate_authority_data" {
   description = "The Kubernetes cluster certificate authority data (module.eks.cluster_ca_certificate)"
-  value       = module.eks.cluster_ca_certificate
+  value       = module.eks.cluster_certificate_authority_data
 }
 
 output "cluster_endpoint" {
   description = "Kubernetes cluster endpoint (module.eks.host)"
-  value       = module.eks.host
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_name" {
