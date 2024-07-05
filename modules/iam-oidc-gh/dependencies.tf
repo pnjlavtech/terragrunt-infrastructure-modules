4b793bca223859d9be4c328ac5ab0fb14cdcb78a
Jay Lavine <jaylavine@gmail.com>
2024-07-04 23:23:50 -0700

    IAM-OIDC-GH module, fix StringLike condition on GH org

--- modules/iam-oidc-gh/dependencies.tf
+++ modules/iam-oidc-gh/dependencies.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "oidc" {
 
     condition {
       test     = "StringLike"
-      values   = ["repo:pnjlavtech/"]
+      values   = ["repo:pnjlavtech/*"]
       variable = "token.actions.githubusercontent.com:sub"
     }
   }
